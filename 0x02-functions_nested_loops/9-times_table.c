6546623ca10c7bd86ab3c28e577623c37eda3800
Brownmelarnie <wemimoajeigbe@gmail.com>
2023-02-21 22:50:00 -0800

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,35 +6,38 @@
 
 void times_table(void)
 {
-	int i, j, k;
+	int a = 0;
+	int b;
+	int rep;
 
-
-	for (i = 0; i < 10; i++)
+	while (a <= 9)
 	{
-		for (j = 0; j < 10; j++)
+		b = 0;
+		while (b <= 9)
 		{
-			k = j * i;
-			if (j == 0)
+			rep = a * b;
+			if (b == 0)
 			{
-				_putchar(k + '0');
+				_putchar('0' + rep);
 			}
-
-			if (k < 10 && j != 0)
+			else if (rep < 10)
+			{
+				_putchar(' ');
+				_putchar('0' + rep);
+			}
+			else
+			{
+				_putchar('0' + rep / 10);
+				_putchar('0' + rep % 10);
+			}
+			if (b < 9)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
 			}
-
-			else if (k >= 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
-			}
+			b++;
 		}
 		_putchar('\n');
+		a++;
 	}
 }
