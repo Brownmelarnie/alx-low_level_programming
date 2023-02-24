76d633109a508b18d17876c78e96b28a6eb84720
Brownmelarnie <wemimoajeigbe@gmail.com>
2023-02-23 18:33:04 -0800

    Isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* _isupper - checks upper case alphas
+* @c: interger parameter
+*
+* Return: something but success
+*/
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
