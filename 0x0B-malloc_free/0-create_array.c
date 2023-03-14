6d02083acc61c9da11eeef92c7a6010840b793ed
Brownmelarnie <wemimoajeigbe@gmail.com>
2023-03-14 15:59:12 -0700

    a function that concatenates all the arguments of your program

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,29 +1,27 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * create_array - create array of size size and assign char c
- * @size: size of array
- * @c: char to assign
- * Description: creat array of size size and assign char c
- * Return: pointer to array, NULL if fail
+ * create_array - creates an array of chars.
+ * @size: size of the array.
+ * @c: storaged char
+ *
+ * Return: pointer of an array of chars
  */
-
 char *create_array(unsigned int size, char c)
 {
-	char *str;
-
+	char *cr;
 	unsigned int i;
 
+	if (size == 0)
+		return (NULL);
 
-	str = malloc(sizeof(char) * size);
+	cr = malloc(sizeof(c) * size);
 
-	if (size == 0 || str == NULL)
-
+	if (cr == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
+		cr[i] = c;
 
-		str[i] = c;
-
-	return (str);
+	return (cr);
 }
